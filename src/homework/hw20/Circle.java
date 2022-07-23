f9ea5110492f485898af74ad655c1d5513acd929
Cosmosiai <akylbekbektenaliev@gmail.com>
2022-07-23 22:45:42 +0600

    Initial commit

--- src/homework/hw20/Circle.java
+++ src/homework/hw20/Circle.java
@@ -1,10 +1,18 @@
 package homework.hw20;
 
 public class Circle<r> {
-//    private final double pi = 3.14;
-//    private double r;
-//
-//    public double S(r){
-//        return r*r*pi;
-//    }
+    private static final double pi = 3.14;
+    private static double r;
+
+    public static void setR(double r) {
+        Circle.r = r;
+    }
+
+    public double S(){
+        return pi*r*r;
+    }
+
+    public static double C(){
+        return 2*r*pi;
+    }
 }
---
+++ src/homework/hw20/Main.java
@@ -0,0 +1,11 @@
+package homework.hw20;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle c =new Circle();
+        Circle.setR(12.2);
+
+        System.out.println(c.S());
+        System.out.println(Circle.C());
+    }
+}
