921432c7b48b2fc9d7e5edaa9581403773a89208
akyl <akylbekbektenaliev@gmail.com>
2022-07-30 20:12:09 +0600

    Initial commit

---
+++ src/homework/hw38/hwA/Main.java
@@ -0,0 +1,20 @@
+package homework.hw38.hwA;
+
+import homework.hw34.hwA.Dalmatian;
+import homework.hw34.hwA.SetSet;
+
+import java.util.HashSet;
+
+
+public class Main {
+    public static void main(String[] args) {
+        HashSet<Dalmatian> a = new HashSet<>();
+        SetSet.setSet(a);
+        int counter = 1;
+        for (Dalmatian d : a){
+            System.out.print(counter+") ");
+            d.readCollar();
+            counter++;
+        }
+    }
+}
